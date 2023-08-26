32a4f4e08a6e069c257c42ac84d130902d17d493
Abiodun-Onajobi Treasure <90309222+treasureTheWhiz@users.noreply.github.com>
2023-08-26 12:27:04 +0000

    Set custom button

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,8 +1,30 @@
+"use client"
 import Image from 'next/image'
+import CustomButton from './CustomButton'
 
 const Hero = () => {
+  const handleScroll=()=>{
+
+  }
   return (
-    <div>Hero</div>
+    <div className='hero'>
+      <div className='flex-1 pt-36 padding-x'></div>
+      <h1 className='hero__title'>
+      The simplest way to create forms
+      </h1>
+      <p className='hero__subtitle'>
+      Create forms for all purposes in seconds.
+      Without knowing how to code, and for free!
+      </p>
+
+      <CustomButton
+      title="Create Form"
+      containerStyles="bg-primary-blue text-white rounded-full mt-10"
+      handleClick={handleScroll}
+
+      />
+
+    </div>
   )
 }
 
